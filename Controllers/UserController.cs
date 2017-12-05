b9453263997f2eec4567039cc2f8fa05e5cd4486
maggs05 <maggs.gose05@hotmail.com>
2017-12-05 10:42:52 -0800

    got the basics working...

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
-// using loginRegistration.Models;
+using loginRegistration.Models;
 // using Microsoft.AspNetCore.Identity;
 namespace loginRegistration.Controllers{
     public class UserController: Controller{
@@ -14,7 +14,14 @@ namespace loginRegistration.Controllers{
     
              return View();
         }
-
-        
+        [HttpPost]
+        [Route("submit")]
+        public IActionResult Register(RegisterUser user){
+            if(ModelState.IsValid){
+                RegisterUser(user);
+                return RedirectToAction("Success");
+            }
+            return View("Index");
+        }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -3,28 +3,37 @@ using System.ComponentModel.DataAnnotations;
 namespace loginRegistration.Models{
     public class RegisterUser{
         [Required]
-        [Display(Name="First Name: ")]
+        [Display(Name="First Name:")]
         [MinLength(2)]
         [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage="First name can only contain letters!")]
         public string FirstName {get; set;}
 
         [Required]
+        [Display(Name="Last Name:")]
         [MinLength(2)]
         [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage="Last name can only contain letters!")]
         public string LastName {get; set;}
 
         [Required]
+        [Display(Name="Email Address:")]
         [EmailAddress]
         public string EmailAddress {get; set;}
 
         [Required]
+        [Display(Name="Password:")]
         [MinLength(8)]
         [DataType(DataType.Password)]
         public string Password {get; set;}
 
+        [Display(Name="Confirm Password")]
         [Compare("Password", ErrorMessage = "Password and confirmation password must match!")]
         public string ConfirmPassword {get; set;}
 
 
-    }   
+    } 
+    public class HomePageUsers
+    {
+        public RegisterUser Register {get;set;}
+        // public LoginUser Login {get;set;}
+    }  
 }
